2653334c69620ae4b8e2fc1e9b9efe23f90c46cc
Cryptic Loop <nickstride1@gmail.com>
2019-03-08 21:10:39 +0000

    Revert to original

--- app.py
+++ app.py
@@ -7,71 +7,11 @@ import os
 app = Flask(__name__)
 
 
-# This information is obtained upon registration of a new GitHub OAuth
-# application here: https://github.com/settings/applications/new
-client_id = r"ZLdQXRHBV3yfp4cztKLiRxItwdiA3ZEHG0e7AbAO"
-client_secret = r"5OukC1OWgdoUexE2AUEnblr0kOlU7HZg0MM0wvFm"
-authorization_base_url = 'https://secure.splitwise.com/oauth/authorize'
-token_url = 'https://secure.splitwise.com/oauth/token'
-
-
 @app.route("/")
 def demo():
-    """Step 1: User Authorization.
-
-    Redirect the user/resource owner to the OAuth provider (i.e. Github)
-    using an URL with a few key OAuth parameters.
-    """
-    github = OAuth2Session(client_id)
-    authorization_url, state = github.authorization_url(authorization_base_url)
-
-    # State is used to prevent CSRF, keep this for later.
-    session['oauth_state'] = state
-    return redirect(authorization_url)
-
-
-# Step 2: User authorization, this happens on the provider.
-
-@app.route("/callback", methods=["GET"])
-def callback():
-    """ Step 3: Retrieving an access token.
-
-    The user has been redirected back from the provider to your registered
-    callback URL. With this redirection comes an authorization code included
-    in the redirect URL. We will use that to obtain an access token.
-    """
-
-    github = OAuth2Session(client_id, state=session['oauth_state'])
-    print("###############################")
-    print("###############################")
-    print("###############################")
-    print("###############################")
-    print(request.url)
-    print("###############################")
-    print("###############################")
-    print("###############################")
-    print("###############################")
-    token = github.fetch_token(token_url,client_id=client_id,client_secret=client_secret,authorization_response=request.url)
-
-    # At this point you can fetch protected resources but lets save
-    # the token and show how this is done from a persisted token
-    # in /profile.
-
-    
-    session['oauth_token'] = token
-
-    return redirect(url_for('.profile'))
-
-
-@app.route("/profile", methods=["GET"])
-def profile():
-    """Fetching a protected resource using an OAuth 2 token.
-    """
-    github = OAuth2Session(client_id, token=session['oauth_token'])
-    return jsonify(github.get('https://www.splitwise.com/api/v3.0/get_current_user').json())
+    return "REN!"
 
 
 if __name__ == "__main__":
 
-    app.secret_key = os.urandom(24)
     app.run(debug=True)
